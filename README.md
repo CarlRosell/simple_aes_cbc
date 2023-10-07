1cf478789d4472e0683c693be87478192992824c
Carl Rosell <me@carlrosell.com>
2023-10-08 00:38:06 +0200

    feat: introduce encryptStringToBase64 and decryptStringFromBase64
    BREAKING: encryptStringToBase64 and decryptStringFromBase64 replaces the old encryptString and decryptString that will no longer perform the base64 encoding/decoding

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # simple_aes_cbc
 
-This is a TypeScript module that provides a simple class for encryption and decryption functionality.
-It uses the `AES-CBC`-algorithm and depends on the global `crypto`, `atob` and `btoa` functions to perform the encryption and decryption, so it can be used in `deno >= 1.15` and `node >= 16.0.0` .
+This is a TypeScript module that provides a simple class for encryption and
+decryption functionality. It uses the `AES-CBC`-algorithm and depends on the
+global `crypto`, `atob` and `btoa` functions to perform the encryption and
+decryption, so it can be used in `deno >= 1.15` and `node >= 16.0.0` .
 
 ## Installation
 
@@ -56,7 +58,10 @@ const stringCrypto = new SimpleAesCbc("1234567890123456", webcrypto.subtle);
 
 ### `new SimpleAesCbc(privateKey: string, subtleCrypto: SubtleCrypto)`
 
-This is the constructor of the `SimpleAesCbc` class. It takes a `privateKey` and a [`subtleCrypto`](https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto) as a parameter, which is used to encrypt and decrypt the data.
+This is the constructor of the `SimpleAesCbc` class. It takes a `privateKey` and
+a
+[`subtleCrypto`](https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto)
+as a parameter, which is used to encrypt and decrypt the data.
 
 #### `encrypt(data: BufferSource): Promise<ArrayBuffer>`
 
@@ -68,11 +73,25 @@ This method decrypts the given data.
 
 #### `encryptString(data: string): Promise<string>`
 
-This will encrypt the given string and wrap transform it to base64.
+Encrypts the given string using the private key.
+The encrypted string returned from this function might not be human readable or used safely in a URL.
+If you want a human readable string that is safe to use, use [`encryptStringSafe`](#encryptstringsafe) instead.
 
 #### `decryptString(data: string): Promise<string>`
 
-This will decrypt the given base64 string and transform it to a normal string.
+Decrypts the given string using the private key.
+This function expects the string to be in the same format as the one returned from [`encryptString`](#encryptstring).
+If you want to decrypt a string that was encrypted using [`encryptStringSafe`](#encryptstringsafe), use [`decryptStringSafe`](#decryptstringsafe) instead.
+
+#### `encryptStringToBase64(data: string): Promise<string>`
+
+Encrypts the given string to a base64 encoded string using the private key.
+The encrypted string returned from this function is human readable and safe to use in a URL.
+
+#### `decryptStringFromBase64(data: string): Promise<string>`
+
+Decrypts the given base64 encoded string using the private key.
+This function expects the string to be in the same format as the one returned from [`encryptStringToBase64`](#encryptstringtobase64).
 
 ## License
 
--- mod.ts
+++ mod.ts
@@ -7,16 +7,16 @@ const ALGORITHM = "AES-CBC";
  * @property {SubtleCrypto} subtle The WebCrypto API.
  */
 export class SimpleAesCbc {
-  private privateKey: string;
+  private privateKey: Uint8Array;
   private subtle: SubtleCrypto;
   private iv: Uint8Array;
   private cryptoKey: CryptoKey | undefined;
 
   constructor(privateKey: string, subtle: SubtleCrypto) {
-    this.privateKey = privateKey;
+    this.privateKey = new TextEncoder().encode(privateKey);
     this.subtle = subtle;
 
-    this.iv = new TextEncoder().encode(privateKey.slice(0, 16));
+    this.iv = this.privateKey.slice(0, 16);
   }
 
   private async getCryptoKey() {
@@ -26,7 +26,7 @@ export class SimpleAesCbc {
 
     this.cryptoKey = await this.subtle.importKey(
       "raw",
-      new TextEncoder().encode(this.privateKey),
+      this.privateKey,
       {
         name: ALGORITHM,
       },
@@ -75,25 +75,55 @@ export class SimpleAesCbc {
 
   /**
    * Encrypts the given string using the private key.
+   * The encrypted string returned from this function might not be human readable or used safely in a URL.
+   * If you want a human readable string that is safe to use, use
+   * {@link SimpleAesCbc#encryptStringToBase64} instead.
    * @param {string} data The data to encrypt.
    * @returns {Promise<string>} The encrypted string.
    * @throws {Error} If the data could not be encrypted.
    */
   async encryptString(data: string): Promise<string> {
     const encrypted = await this.encrypt(new TextEncoder().encode(data));
-    return btoa(String.fromCharCode(...new Uint8Array(encrypted)));
+    return String.fromCharCode(...new Uint8Array(encrypted));
   }
 
   /**
    * Decrypts the given string using the private key.
+   * This function expects the string to be in the same format as the one returned from
+   * {@link SimpleAesCbc#encryptString}.
+   * If you want to decrypt a string that was encrypted using
+   * {@link SimpleAesCbc#encryptStringToBase64}, use {@link SimpleAesCbc#decryptStringFromBase64} instead.
    * @param {string} data The data to decrypt.
    * @returns {Promise<string>} The decrypted string.
    * @throws {Error} If the data could not be decrypted.
    */
   async decryptString(data: string): Promise<string> {
     const decrypted = await this.decrypt(
-      Uint8Array.from(atob(data), (c) => c.charCodeAt(0))
+      Uint8Array.from(data, (c) => c.charCodeAt(0))
     );
     return new TextDecoder().decode(decrypted);
+  }
+
+  /**
+   * Encrypts the given string to a base64 encoded string using the private key.
+   * The encrypted string returned from this function is human readable and safe to use in a URL.
+   * @param {string} data The data to encrypt.
+   * @returns {Promise<string>} The encrypted string in base64 format.
+   * @throws {Error} If the data could not be encrypted.
+   */
+  async encryptStringToBase64(data: string): Promise<string> {
+    return btoa(await this.encryptString(data));
+  }
+
+  /**
+   * Decrypts the given base64 encoded string using the private key.
+   * This function expects the string to be in the same format as the one returned from
+   * {@link SimpleAesCbc#encryptStringToBase64}.
+   * @param {string} data The data to decrypt.
+   * @returns {Promise<string>} The decrypted string.
+   * @throws {Error} If the data could not be decrypted.
+   */
+  async decryptStringFromBase64(data: string): Promise<string> {
+    return await this.decryptString(atob(data));
   }
 }
